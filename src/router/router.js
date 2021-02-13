17d5db404778c7eddd3defdc14fb2393d8d58964
saadshahid1423 <saadshahid1423@gmail.com>
2021-02-13 17:15:18 +0500

    second commit

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {BrowserRouter as Router,Route,} from "react-router-dom";
 import Start from '../screens/start/start';
 import First from '../screens/first/first';
+import Second from '../screens/second/second';
+
 class AppRouter extends React.Component{
     render(){
         return(
             <Router>
                 <Route exact path="/" component={Start} />
                 <Route path="/welcome" component={First} />
+                <Route path="/night" component={Second} />
             </Router>
         )
     }
